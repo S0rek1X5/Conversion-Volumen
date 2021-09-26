73cd86d3323c2e327335dff19a10dc7e75dc7612
Daniel <danielbebon@gmail.com>
2021-09-25 21:03:39 -0500

    Litros a galones
    Le añadi la funcion de poder convertir galones a litros

--- Conversion galones a litros.c
+++ Conversion galones a litros.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 int main ()
 {
-	//Bloque declarativo de variables
 	float galones, litros;
-	//Bloque de instrucciones
+	int conv;
+	
+	printf("Coversion galones a litros y litros a galones");
+	printf("Galones a litros-->1\nLitros a galones-->2\n:");
+	scanf("%d",&conv);
+	if(conv==1){
 	printf("\nIntroduzca su volumen:");
 	scanf("%f", &galones);
-	//Conversion a litro
 	litros = galones * 3.785;
 	printf ("\nLa cantidad en litros es:%1.2f", litros);
-	return 0;
 	}
+	else if(conv==2){
+		printf("\nIntroduzca su volumen:");
+		scanf("%f", &litros);
+		galones = litros / 3.785;
+		printf ("\nLa cantidad en galones es:%1.2f", galones);
+	}
+		
+	
+	return 0;
+}
